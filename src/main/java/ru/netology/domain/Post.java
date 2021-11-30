a6f9bae7426631c268b3e7a82c4b4debee33b256
Yuliya Shkuratko <sm0ci@mail.ru>
2021-11-30 23:08:02 +0300

    edit Post.java

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,22 +1,61 @@
 package ru.netology.domain;
 
 public class Post {
+    private int id;
     private int ownerId;
-    private int data;
-    private boolean hide;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean isPinned;
+    private boolean markedAsAds;
+    private boolean isFavorite;
 
-    public class Banner {
-        private String text;
-        private boolean save;
-        private int idUrl;
 
-        public class Interaction {
-            private int likes;
-            private String comments;
-            private int reposts;
-            private int views;
+    public class Comments {
+        private int count;
+        private boolean canPost;
+        private boolean groupsCanPost;
+        private boolean canClose;
+        private boolean canOpen;
 
-            //             + get/set на все поля
+        public class Copyright {
+            private int id;
+            private String link;
+            private String name;
+            private String type;
+
+            public class Likes {
+                private int count;
+                private boolean userLikes;
+                private boolean canLikes;
+                private boolean canPublish;
+
+                public class Reposts {
+                    private int count;
+                    private boolean userReposted;
+
+                    public class Views {
+                        private int count;
+
+                        public class Donut {
+                            private boolean isDonut;
+                            private int paidDuration;
+                            private boolean canPublishFreeCopy;
+                            private String editMode;
+                        }
+
+
+                        //             + get/set на все поля
+                    }
+                }
+            }
         }
     }
 }
